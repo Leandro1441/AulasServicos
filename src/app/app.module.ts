f421b2008b5d6fe5fa211c553ebb75636100eb6f
Leandro Silva <leandronstrab@gmail.com>
2019-11-07 13:50:37 -0300

    Injeção de dependeencia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursosService } from './cursos/cursos.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
